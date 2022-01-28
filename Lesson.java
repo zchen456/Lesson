bb85ab9c9291e7c9d7ed47fd8fc268d7de6b9f27
Yuan Li <liyu4@oregonstate.edu>
2022-01-27 22:13:39 -0800

    Lesson 10

--- Lesson.java
+++ Lesson.java
@@ -231,7 +231,25 @@ class Lesson9{
 }
 
 
-
+/*
+一个数如果恰好等于它的因子只和，这个数就称为”完数“。例如6=1+2+3.
+编程：找出1000以内的所有完数。
+*/
+class Lesson10{
+	public static void main(String[] args){
+		for(int i = 1;i <= 1000;i++){
+			int sum = 0;
+			for(int j = 1;j <= i/2;j++){
+				if(i % j == 0){
+					sum += j;
+				}
+			}	
+			if(i == sum){
+				System.out.println(i);
+			}
+		}
+	}
+}
 
 
 
